70df040a95f9228f33cb6b54ad8e949479bd9506
Manish Kumar <manishkumar@xebia.com>
2019-09-24 21:46:33 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# pr-kudos-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A bot to motivate and make you feel good when you raise a PR
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how pr-kudos-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2019 Manish Kumar <manis.kasyap@gmail.com> (https://medium.com/all-is-web)
---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+  app.on('pull_request.opened', async context => {
+    const promment = context.issue({ body: 'Thanks for opening this PR!' })
+    return context.github.pull_request.createComment(promment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
